4a1d77523ea31451430d1053e3ace20074384330
Ichihiroy <realyanisquliyev@gmail.com>
2025-11-11 23:17:22 +0400

    refactor: update main function to use print module; adjust dependencies

--- target/debug/deps/Rust-13df9641794cab7d.d
+++ target/debug/deps/Rust-13df9641794cab7d.d
@@ -1,5 +1,6 @@
-C:\Users\ASUS\Desktop\Projects\Rust\target\debug\deps\Rust-13df9641794cab7d.d: src\main.rs
+C:\Users\ASUS\Desktop\Projects\Rust\target\debug\deps\Rust-13df9641794cab7d.d: src\main.rs src\print.rs
 
-C:\Users\ASUS\Desktop\Projects\Rust\target\debug\deps\libRust-13df9641794cab7d.rmeta: src\main.rs
+C:\Users\ASUS\Desktop\Projects\Rust\target\debug\deps\libRust-13df9641794cab7d.rmeta: src\main.rs src\print.rs
 
 src\main.rs:
+src\print.rs:
--- target/debug/deps/Rust-454569f8e25cc6a1.d
+++ target/debug/deps/Rust-454569f8e25cc6a1.d
@@ -1,5 +1,6 @@
-C:\Users\ASUS\Desktop\Projects\Rust\target\debug\deps\Rust-454569f8e25cc6a1.d: src\main.rs
+C:\Users\ASUS\Desktop\Projects\Rust\target\debug\deps\Rust-454569f8e25cc6a1.d: src\main.rs src\print.rs
 
-C:\Users\ASUS\Desktop\Projects\Rust\target\debug\deps\libRust-454569f8e25cc6a1.rmeta: src\main.rs
+C:\Users\ASUS\Desktop\Projects\Rust\target\debug\deps\libRust-454569f8e25cc6a1.rmeta: src\main.rs src\print.rs
 
 src\main.rs:
+src\print.rs:
