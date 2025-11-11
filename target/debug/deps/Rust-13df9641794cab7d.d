18f06f0a803a25fa3322542f9f2fb670562c14eb
Ichihiroy <realyanisquliyev@gmail.com>
2025-11-11 22:58:33 +0400

    Add incremental compilation artifacts and update build outputs
    - Created new query-cache.bin and work-products.bin files for incremental compilation.
    - Added a lock file for the incremental compilation state.
    - Updated stderr and stdout files to reflect the successful completion of the build process.

---
+++ target/debug/deps/Rust-13df9641794cab7d.d
@@ -0,0 +1,5 @@
+C:\Users\ASUS\Desktop\Projects\Rust\target\debug\deps\Rust-13df9641794cab7d.d: src\main.rs
+
+C:\Users\ASUS\Desktop\Projects\Rust\target\debug\deps\libRust-13df9641794cab7d.rmeta: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/Rust-454569f8e25cc6a1.d
@@ -0,0 +1,5 @@
+C:\Users\ASUS\Desktop\Projects\Rust\target\debug\deps\Rust-454569f8e25cc6a1.d: src\main.rs
+
+C:\Users\ASUS\Desktop\Projects\Rust\target\debug\deps\libRust-454569f8e25cc6a1.rmeta: src\main.rs
+
+src\main.rs:
